b6cef958a1ea5245f90ebd169af98637f3a52b09
jinlingjian <jinlingjian@hikvision.com.cn>
2020-11-24 16:30:54 +0800

    modify B

--- src/main/java/com/kk/B.java
+++ src/main/java/com/kk/B.java
@@ -1,4 +1,10 @@
 package com.kk;
 
 public class B {
+    public static void main(String[] args) {
+        System.out.println("developing B");
+    }
+    public String get(){
+        return "1";
+    }
 }
